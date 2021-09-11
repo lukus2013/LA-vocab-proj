8559781639b981acf021319043bb41e443a6f14c
unknown <lukus2013@yahoo.com>
2021-09-11 13:51:28 -0500

    2nd commit

--- src/scripts/components/startApp.js
+++ src/scripts/components/startApp.js
@@ -4,11 +4,13 @@ import { getWords } from '../helpers/data/wordData';
 import showWords from '../helpers/showWords';
 import navBar from '../helpers/dom/navBar';
 import navEvents from '../helpers/events/navEvents';
+import logoutButton from './logoutButton';
 
 const startApp = () => {
   createDom();
   domClicks();
   navBar();
+  logoutButton();
   navEvents();
   getWords().then((words) => showWords(words));
 };
